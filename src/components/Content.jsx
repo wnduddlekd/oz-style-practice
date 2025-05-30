7c0e87cb7cb60fb5825a5ca9a2f748959f1fc9e2
wndud <ju03209@naver.com>
2025-05-30 15:51:50 +0900

    style: Content 컴포넌트 styled-components로 스타일 변경

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,43 @@
+import styled from "styled-components";
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 5px;
+`;
+
+const ContainerImg = styled.img`
+  width: 300px;
+  border-radius: 10px;
+  margin-bottom: 3px;
+`;
+
+const ContainerSpan = styled.span`
+  font-size: 12px;
+  color: #d7fa00;
+  border: 1px solid #d7fa00;
+  padding: 4px 5px;
+  border-radius: 3px;
+`;
+
+const ContainerDiv = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const ContainerP = styled.p`
+  font-size: 12px;
+  color: rgb(160, 160, 160);
+`;
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
-    </div>
+    <ContentContainer>
+      <ContainerImg src={content.img} alt={content.title} />
+      <ContainerSpan>모집중</ContainerSpan>
+      <ContainerDiv>{content.title}</ContainerDiv>
+      <ContainerP>{content.subtitle}</ContainerP>
+    </ContentContainer>
   );
 }
