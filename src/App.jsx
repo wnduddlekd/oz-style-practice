46712ca1b0fb4b2b13292bbda85d89562abb42b9
wndud <ju03209@naver.com>
2025-05-30 16:15:33 +0900

    style: App 컴포넌트 styled-components로 스타일 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import "./App.scss";
+import styled from "styled-components";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+
+const Section = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </Section>
     </main>
   );
 }
