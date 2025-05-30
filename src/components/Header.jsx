2d6ae77eb1d6b032ddbd4d72646b6e2397f14f46
wndud <ju03209@naver.com>
2025-05-30 16:14:55 +0900

    style: Header 컴포넌트 styled-components로 스타일 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,39 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: black;
+`;
+
+const HeaderH1 = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const HeaderUl = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  list-style: none;
+  font-weight: 400;
+  font-size: 13px;
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
-      <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
+    <HeaderContainer>
+      <HeaderH1>OZ코딩스쿨</HeaderH1>
+      <HeaderUl>
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
-      </ul>
-    </header>
+      </HeaderUl>
+    </HeaderContainer>
   );
 }
